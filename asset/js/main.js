5e2e547c8fd447f9a904580a2699c32c72cbc95d
vikas6938 <vikasborse000@email.com>
2023-04-07 15:14:37 +0530

    first commit

---
+++ asset/js/main.js
@@ -0,0 +1,10 @@
+
+
+//--Modal start
+
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
